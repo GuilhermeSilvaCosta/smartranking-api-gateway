e956763b2c8ee4d55091c9858fd128bf501c298d
Guilherme Costa <guilhermecosta@MacBook-Pro-de-Guilherme.local>
2024-02-12 17:53:20 -0300

    Add timeout interceptor

---
+++ src/interceptors/logging.interceptor.ts
@@ -0,0 +1,19 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    console.time('request time');
+
+    return next.handle().pipe(tap(() => console.timeEnd('request time')));
+  }
+}
